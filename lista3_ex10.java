5c04ca1f58603538cbee9d1933249203ab143aa1
gabriel garcia <gabrielmgarcia96@gmail.com>
2022-03-31 13:15:10 -0300

    Add files via upload

---
+++ lista3_ex10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class lista3_ex10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe homem(1) ou mulher(2): ");
+        int userGender = scanner.nextInt();
+
+        if (userGender == 1) {
+            System.out.println("Agora que já sabemos que você é homem informe altura: " );
+            double userHeight = scanner.nextDouble();
+
+            double idealWeight = 72.7 * userHeight - 58;
+            System.out.printf("Seu peso ideal é:%.2f ", idealWeight + "kg");
+        } else if (userGender == 2) {
+            System.out.println("Agora que já sabemos que você é mulher informe altura: " );
+            double userHeight = scanner.nextDouble();
+
+            double idealWeight = 62.1 * userHeight - 44.7;
+            System.out.printf("Seu peso ideal é:%.2f ", idealWeight + "kg");
+        }
+        
+        scanner.close();
+    }
+}
