2eb2d4b7d0ec77dd4a6b0d27364d609b5dd22d21
gabriel garcia <gabrielmgarcia96@gmail.com>
2022-04-19 12:56:54 -0300

    Add files via upload

---
+++ IFRS_logica2022/numeroPalindromo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class numeroPalindromo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("digite um valor de 4 digitos: ");
+        int numeroInteiro = scanner.nextInt();
+
+        int primeiroAlgarismo = numeroInteiro / 1000;
+
+        int sobraPrimeiroAlgarismo = numeroInteiro % 1000;
+        int segundoAlgarismo = sobraPrimeiroAlgarismo / 100;
+
+        int terceiroAlgarismo = (sobraPrimeiroAlgarismo % 100) / 10;
+
+        int quartoAlgarismo = (sobraPrimeiroAlgarismo % 10);
+
+        System.out.print(quartoAlgarismo + "" + terceiroAlgarismo + "" + segundoAlgarismo + "" + primeiroAlgarismo);
+    }
+}
